c65b9184b930aa6663b2d7f8e570cede02c9cba6
Hans Ole Hatzel <hhatzel@gmail.com>
2016-01-26 00:05:14 +0100

    Options for gen-pdb.

--- Sources/gen-pdb/main.swift
+++ Sources/gen-pdb/main.swift
@@ -2,10 +2,25 @@ import SlidingPuzzle
 import Commander
 
 let main = command(
-    Option("size", 100_000)
-) { size in
-    let pf = PatternFinder(startBoard: BoardState())
-    pf.search(size)
+    Option("size", 100_000),
+    VaradicArgument<String>("patterns", description: "Space seperated pattern names to generate")
+) { size, pattern_strings in
+    var patterns: [[UInt8]] = []
+    if pattern_strings.count == 0 {
+        patterns.append([0,1,2,3,4,8,12])
     }
+    for p in pattern_strings {
+        switch p {
+            case "fringe":
+                patterns.append([0,1,2,3,4,8,12])
+            case "corner":
+                patterns.append([0,1,2,3,5,6,7])
+            default:
+                continue
+        }
+    }
+    let pf = PatternFinder(startBoard: BoardState())
+    pf.search(size, patternDefinitions: patterns)
+}
 
 main.run()
