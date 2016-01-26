e3d782f1fc79c3e1dbbcdae276eb94d630b06846
Hans Ole Hatzel <hhatzel@gmail.com>
2016-01-26 12:34:26 +0100

    Moved pattern definitions into constants.

--- Sources/gen-pdb/main.swift
+++ Sources/gen-pdb/main.swift
@@ -1,20 +1,25 @@
 import SlidingPuzzle
 import Commander
 
+class RelevantElements {
+    static let fringe = Array<UInt8>([0,1,2,3,4,8,12])
+    static let corner = Array<UInt8>([0,1,2,3,5,6,7])
+}
+
 let main = command(
     Option("size", 100_000),
     VaradicArgument<String>("patterns", description: "Space seperated pattern names to generate")
 ) { size, pattern_strings in
     var patterns: [[UInt8]] = []
     if pattern_strings.count == 0 {
-        patterns.append([0,1,2,3,4,8,12])
+        patterns.append(RelevantElements.fringe)
     }
     for p in pattern_strings {
         switch p {
             case "fringe":
-                patterns.append([0,1,2,3,4,8,12])
+                patterns.append(RelevantElements.fringe)
             case "corner":
-                patterns.append([0,1,2,3,5,6,7])
+                patterns.append(RelevantElements.corner)
             default:
                 continue
         }
